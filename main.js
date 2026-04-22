e8d3d7082c0120569c8f234b54b781d2596b4119
David Santamaría Rogado <howl.nsp@gmail.com>
2026-04-22 10:23:46 +0200

    main.js: add second function puedeVotar.
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- main.js
+++ main.js
@@ -16,3 +16,20 @@ console.log(verificarNumero(0));
 console.log(verificarNumero(3));
 console.log(verificarNumero(-5));
 console.log(verificarNumero(8));
+
+/**
+ * Devuelve si con la edad dada puede votar o no.
+ * @param {number} edad - La edad a verificar.
+ * @returns {string} - Mensaje que indica si puede o no votar.
+ */
+function puedeVotar(edad) {
+  if (edad >= 18)
+    return 'Puede votar';
+  else
+    return 'No puede votar';
+}
+
+console.log(puedeVotar(23));
+console.log(puedeVotar(5));
+console.log(puedeVotar(18));
+console.log(puedeVotar(56));
